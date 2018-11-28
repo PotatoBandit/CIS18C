49c1b33f308dd591004e341012884f8966e974e0
Rebecca C <205rcarter@gmail.com>
2018-11-27 18:52:35 -0800

    Update Department.java

--- Final Project/Department.java
+++ Final Project/Department.java
@@ -1,12 +1,14 @@
 
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.*;
 
 public class Department 
 {
     int id;
     String description;
-   HashMap<Integer, Employee>  map;
+   HashMap<Integer, Employee>  map = new HashMap();
 
    Department(int id, String description)
    {
@@ -59,14 +61,32 @@ public class Department
    void addEmployee(Integer id,  String name, String bday, String ss,  String salary,  String address)
    {
          Employee newEmp = new Employee(id, name, bday, ss, salary, address);
-         map.put(id, newEmp);
+         map.put(newEmp.getEmployee_ID(), newEmp);
    }
    
-   void populateMap()
+   void populateDept()
    {
-       addEmployee(1, "Scott", "10/10/10" , "12-12-1232", "2123124", "132413 asdfadfadf " );
+       addEmployee(1, "Scott", "10/10/10" , "12-12-1232", "2123124", "908 Goodman St. Scranton, PA. 18512 " );
+       addEmployee(2, "Scott", "10/10/10" , "12-12-1232", "2123124", "110 E Main  St. Hummelstown, PA. 17036 " );
+       addEmployee(3, "Scott", "10/10/10" , "12-12-1232", "2123124", "911 Hamilton Ave Farrell, PA. 16121 " );
+       addEmployee(4, "Scott", "10/10/10" , "12-12-1232", "2123124", "910 Cedar Ave Scranton, PA. 18505 " );
+       addEmployee(5, "Scott", "10/10/10" , "12-12-1232", "2123124", "1396 Hall Ave Sharon, PA. 16146 " );
+       addEmployee(6, "Scott", "10/10/10" , "12-12-1232", "2123124", "913 Eynon St. Scranton, PA. 18504" );
+       addEmployee(7, "Scott", "10/10/10" , "12-12-1232", "2123124", "" );
+       addEmployee(8, "Scott", "10/10/10" , "12-12-1232", "2123124", "132413 asdfadfadf " );
+       addEmployee(9, "Scott", "10/10/10" , "12-12-1232", "2123124", "132413 asdfadfadf " );
+       addEmployee(10, "Scott", "10/10/10" , "12-12-1232", "2123124", "132413 asdfadfadf " );
+                
    }
    
-   
+   void printEmployeeList()
+   {
+       populateDept();
+      Set<Entry<Integer, Employee>> hashSet = map.entrySet();
+      for(Entry entry:  hashSet)
+      {
+          System.out.println("key="+ entry.getKey()+", Value= " + entry.getValue());
+      }
+   }
    
 }
